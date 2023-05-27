453e83a6e8a6dde77b7579b733161b9693ec7f85
Juan David Castro <me@juandc.co>
2023-05-27 11:07:28 -0500

    Add TypeScript

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+// import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next';
+
+function Home() {
+  return (
+    <p>Home</p>
+  );
+}
+
+export default Home;
+ 
+// export const getStaticProps: GetStaticProps = async (context) => { };
+// export const getStaticPaths: GetStaticPaths = async () => { };
+// export const getServerSideProps: GetServerSideProps = async (context) => { };
