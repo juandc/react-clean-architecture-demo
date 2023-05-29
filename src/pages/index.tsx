49f363bd6955ed6c2fed1a053ff2c738f39ac3cd
Juan David Castro <me@juandc.co>
2023-05-29 07:29:49 -0500

    Add Next Link Wrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,10 +2,12 @@ import { GetServerSideProps } from 'next';
 import { Salute } from '@/modules/salute/salute.types'
 import { createSaluteAdapter } from '@/modules/salute/salute.adapter';
 import { SaluteHTTPData } from '@/modules/salute/salute.data';
+import { Link } from '@/components/Link';
 
 export default function HomePage({ salute }) {
   return (
     <>
+      <Link href="/liked">Liked</Link>
       <h1>Home</h1>
       <p>Lista de cosas (from redux, aunque no lo sepa)</p>
       <p>Data: '{salute}'</p>
